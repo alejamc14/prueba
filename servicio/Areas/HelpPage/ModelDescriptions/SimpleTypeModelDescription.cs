655922e5272baf1642de3c06542e41e164d0cf50
alejamc14 <alejandra.madrid1426@gmail.com>
2024-11-14 21:12:19 -0500

    inicial

---
+++ servicio/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace servicio.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ servicio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="servicio.WebApiApplication" Language="C#" %>
